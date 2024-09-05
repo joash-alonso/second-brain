5993e9a56df8b292ce9aab6b6ed34d0d9ab109b5
Joash <joashalonso@gmail.com>
2024-09-05 19:35:30 +0100

    feat: add negative space statements

--- backend/services/extraction.py
+++ backend/services/extraction.py
@@ -11,13 +11,28 @@ class BaseExtractor(ABC):
 
 
 class SimplePDFExtractor(BaseExtractor):
-    def __init__(self, path: Path | str, num_workers: int | None = None) -> None:
-        self.path = path
+    def __init__(
+        self, file_paths: List[Path | str], num_workers: int | None = None
+    ) -> None:
+        self.file_paths = file_paths
         self.num_workers = num_workers
+        self.__required_exts = ["pdf"]
 
     @property
     def pdf_loader(self):
-        return SimpleDirectoryReader(input_dir=self.path)
+        if not isinstance(self.file_paths, list):
+            raise TypeError("File paths should be a list")
+
+        return SimpleDirectoryReader(
+            input_files=self.file_paths, required_exts=self.__required_exts
+        )
 
     def extract(self) -> List[Document]:
-        return self.pdf_loader.load_data(self.path, num_workers=self.num_workers or 4)
+        return self.pdf_loader.load_data(
+            self.file_paths, num_workers=self.num_workers or 4
+        )
+
+
+if __name__ == "__main__":
+    extractor = SimplePDFExtractor("sample.pdf")
+    print(extractor.extract())
